87c7460183b4d5380e136cff143d1f39beb433d6
Muhammad Aulia Rahman <muharmaann@gmail.com>
2025-12-11 07:58:45 +0700

    update

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,18 @@
-import "../styles/global.css";
+import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [qc] = useState(() => new QueryClient());
+  const [qc, setQc] = useState<QueryClient | null>(null);
+
+  useEffect(() => {
+    setQc(new QueryClient());
+  }, []);
+
+  if (!qc) return null;
+
   return (
     <QueryClientProvider client={qc}>
       <Component {...pageProps} />
