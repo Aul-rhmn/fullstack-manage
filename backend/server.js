f29369cd5b4ba7dcbd067368c5f7d18e1feda201
Muhammad Aulia Rahman <muharmaann@gmail.com>
2025-12-11 16:22:45 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
-app.use(cors({ origin: 'https://fullstack-manage-1.onrender.com' }));
-
+const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config();
+
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "https://fullstack-manage-1.onrender.com",
+}));
+
 app.use(express.json());
 
-// static uploads
 app.use(
   "/uploads",
   express.static(path.join(__dirname, process.env.UPLOAD_DIR || "uploads"))
@@ -19,10 +22,10 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const employeeRoutes = require("./routes/employeeRoutes");
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/employees", employeeRoutes);
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log("Server running on", port));
